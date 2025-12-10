39958624f3ca65306cba323eeb65ec098ed99a1a
iotvagin <iotvagin1044@gmail.com>
2025-12-10 19:32:14 +0300

    TESTED: Now passing register_and_get_tokens postman tests!!!! Finish custom user/user_create and token_create serializers based on djoser.serializers

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,28 +1,72 @@
 from django.contrib.auth import get_user_model
 
+from djoser import serializers as djoser_serializers
+
 from rest_framework import serializers
 from rest_framework import validators
 
 User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
+# Just setting a bunch of fields as required and checking email uniqueness
+class UserSerializer(djoser_serializers.UserCreateSerializer):
     email = serializers.EmailField(
         validators=[validators.UniqueValidator(queryset=User.objects.all())])
 
-    class Meta:
-        model = User
+    class Meta(djoser_serializers.UserCreateSerializer.Meta):
         fields = (
+            "id",
             "email",
             "username",
+            "password",
             "first_name",
             "last_name",
             # "is_subscribed",
             # "avatar",
         )
-        required_fields = (
-            "email",
-            "username",
-            "first_name",
-            "last_name",
-        )
+        extra_kwargs = {
+            "password": {
+                "write_only": True
+            },
+            "email": {
+                "required": True
+            },
+            "username": {
+                "required": True
+            },
+            "first_name": {
+                "required": True
+            },
+            "last_name": {
+                "required": True
+            },
+        }
+
+
+# Requires email+password combination instead of default username+password
+class TokenCreateSerializer(djoser_serializers.TokenCreateSerializer):
+
+    class Meta:
+        fields = ("email", "password")
+        extra_kwargs = {
+            "email": {
+                "required": True,
+            },
+        }
+
+    def validate(self, attrs):
+        cred_err = djoser_serializers.ValidationError(
+            "Invalid credentials provided.")
+
+        # finding the user
+        try:
+            self.user = User.objects.get(email=attrs.get("email", None))
+        except Exception:
+            raise cred_err
+
+        if not self.user.check_password(attrs.get("password", None)):
+            raise cred_err
+
+        # setting attrs["user"] so it would appear in serializer.validated_data
+        attrs["user"] = self.user
+        return attrs
