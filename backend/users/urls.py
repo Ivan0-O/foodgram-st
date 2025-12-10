68a8eb302b221dca9ce33da2a745c8975af72401
iotvagin <iotvagin1044@gmail.com>
2025-12-10 23:25:04 +0300

    remove comment 2

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -6,8 +6,6 @@ from .views import UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r"users", UserViewSet)
-# router.register(r"recipes", RecipeViewSet)
-# router.register(r"ingredients", IngredientViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
