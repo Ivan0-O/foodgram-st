1b231d2ca33f71ff0e952f766fb961bca7017efb
iotvagin <iotvagin1044@gmail.com>
2025-12-11 18:01:36 +0300

    Fix permission verification for /users/me (unathorized user gets 401 now

--- backend/users/views.py
+++ backend/users/views.py
@@ -16,11 +16,20 @@ class UserViewSet(djoser_views.UserViewSet):
 
     pagination_class = pagination.LimitOffsetPagination
 
+    # NOT NEEDED NOW. For reference only
     # filter_backends = [DjangoFilterBackend,
     #                    filters.SearchFilter, filters.OrderingFilter]
     # filterset_fields = ("color", "birth_year")
     # search_fields = ("$name", "owner__username")
     # ordering_fields = ("name", "birth_year")
+
+    def get_permissions(self):
+        # Refuse if an anonymous user tries to get the /users/me/
+        if self.action == "me":
+            return [permissions.IsAuthenticated()]
+
+        # Otherwise use the permission_classes default
+        return super().get_permissions()
 
     def get_queryset(self):
         return User.objects.all().order_by("username")
