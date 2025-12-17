eabfca6d0d25a7594bd1832ae50b28e0df0252d8
iotvagin <iotvagin1044@gmail.com>
2025-12-17 23:08:44 +0300

    remove redoc from backend

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,14 +1,8 @@
-from django.conf import settings
-from django.conf.urls.static import static
-
 from django.contrib import admin
 from django.urls import include, path
 
-from django.views.generic import TemplateView
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-
     path("s/", include("shortlinks.urls")),
     path("api/", include("users.urls")),
     path("api/", include("recipes.urls")),
@@ -17,12 +11,3 @@ urlpatterns = [
     path("api/auth/", include("djoser.urls.authtoken")),
     path("api/auth/", include("djoser.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-    urlpatterns += [
-        path("api/docs/",
-             TemplateView.as_view(template_name="redoc.html"),
-             name="redoc"),
-    ]
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework import viewsets, mixins, permissions, pagination, status
+from rest_framework import viewsets, mixins, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
