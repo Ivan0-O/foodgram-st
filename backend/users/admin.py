e6ae40d4aeed0e6a3ff5110811b02562635da6e2
iotvagin <iotvagin1044@gmail.com>
2025-12-21 20:45:52 +0300

    tweak user

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -6,12 +6,7 @@ from .models import User, Subscription
 @admin.register(User)
 class UserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (("Аватар", {"fields": ("avatar", )}), )
-
-# @admin.register(Ingredient)
-# class IngredientAdmin(admin.ModelAdmin):
-#     list_display = ("name", "measurement_unit")
-#     search_fields = ("name", "measurement_unit")
-#     list_filter = ("measurement_unit", )
+    search_fields = ("username", "email")
 
 
 @admin.register(Subscription)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,12 +1,40 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from foodgram_backend.constants import (
+    USER_USERNAME_MAX_LENGTH,
+    USER_AVATAR_PATH,
+    USER_EMAIL_MAX_LENGTH,
+    USER_FIRST_NAME_MAX_LENGTH,
+    USER_LAST_NAME_MAX_LENGTH,
+)
 
+
+# logging in is done via an email+password combination
+# see TokenCreateSerializer from api.views
 class User(AbstractUser):
     # make email unique
-    email = models.EmailField(unique=True, max_length=254)
-    avatar = models.ImageField(verbose_name="Аватар",
-                               upload_to="users/",
+    email = models.EmailField(verbose_name="Электронная почта",
+                              max_length=USER_EMAIL_MAX_LENGTH,
+                              unique=True,
+                              )
+
+    username = models.CharField(verbose_name="Имя пользователя",
+                                max_length=USER_USERNAME_MAX_LENGTH,
+                                unique=True,
+                                db_index=True,
+                                )
+
+    first_name = models.CharField(verbose_name="Имя",
+                                  max_length=USER_FIRST_NAME_MAX_LENGTH,
+                                  )
+
+    last_name = models.CharField(verbose_name="Фамилия",
+                                 max_length=USER_LAST_NAME_MAX_LENGTH,
+                                 )
+
+    avatar = models.ImageField(verbose_name="Аватар пользователя",
+                               upload_to=USER_AVATAR_PATH,
                                null=True,
                                blank=True,
                                default=None)
@@ -14,6 +42,9 @@ class User(AbstractUser):
     class Meta(AbstractUser.Meta):
         verbose_name = "пользователь"
         verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.username
 
 
 class Subscription(models.Model):
