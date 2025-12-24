ab988c72b0ccca996d62c8436e39e13d73554a2e
iotvagin <iotvagin1044@gmail.com>
2025-12-24 15:26:52 +0300

    change is_staff to is_superuser

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -8,4 +8,4 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
             return True
 
         return request.user.is_authenticated and (obj.author == request.user
-                                                  or request.user.is_staff)
+                                                  or request.user.is_superuser)
